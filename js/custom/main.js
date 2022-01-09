532f0635f49beba2f11d5ac330dfaaffb07b14fa
Aleksey Kotylev <leksrus>
2022-01-08 22:01:42 -0300

    checkout and close order features

--- js/custom/main.js
+++ js/custom/main.js
@@ -158,6 +158,16 @@ function showCart() {
 
   if (cartElment.length > 0) {
     const cartItemElements = $("#cart-list > .align-items-start");
+    $("#checkout-btn").click((e) => {
+      e.preventDefault();
+      checkOut();
+    });
+
+    $("#finish-order").click((e) => {
+      e.preventDefault();
+      closeOrder();
+    });
+
     cartItemElements.remove();
     const hrElement = $("#cart-list > hr").first().next();
     const cart = setUpCartObject();
@@ -180,7 +190,7 @@ function showCart() {
                   <div class="card-body font-varela">
                     <h5 class="card-title"><b>${product.name}</b></h5>
                     <p class="card-text">${product.description}</p>
-                    <p class="card-text cart-product-price">${product.price}</p>
+                    <p id="price-${product.id}"  class="card-text cart-product-price">$${product.price}</p>
                     <div class="d-flex align-items-center justify-content-center">
                       <div id="plus-${product.id}" class="btn-cart">+</div>
                       <div class="count">${count}</div>
@@ -204,35 +214,42 @@ function showCart() {
           `
         );
 
-        //bind event to remove item from cart
-        cartElment.find(`#remove-${product.id}`).click(() => {
-          $("#cart-list").find(`#${this.event.target.id}`).closest(".row.align-items-start").remove();
-          removeProductFromCart(parseInt(this.event.target.id));
-        });
-
-        //bind event to add item from cart by plus button
-        cartElment.find(`#plus-${product.id}`).click(() => {
-          const productId = parseInt(this.event.target.id.split("-")[1]);
-          const prodCount = parseInt(productCount(productId));
-          $(`#${this.event.target.id}`)
-            .next(".count")
-            .text(`${prodCount + 1}`);
-          addProduct(productId);
-        });
-
-        //bind event to remove item from cart by minus button
-        cartElment.find(`#minus-${product.id}`).click(() => {
-          const productId = parseInt(this.event.target.id.split("-")[1]);
-          const prodCount = parseInt(productCount(productId));
-          $(`#${this.event.target.id}`)
-            .prev(".count")
-            .text(`${prodCount - 1}`);
-          removeProductFromCart(productId);
-          if (prodCount - 1 === 0) $("#cart-list").find(`#${productId}`).closest(".row.align-items-start").remove();
-        });
+        //bind event cart
+        bindCartEvents(product, cartElment);
       }
     }
   }
+}
+
+function bindCartEvents(product, cartElment) {
+  //bind event to remove item from cart
+  cartElment.find(`#remove-${product.id}`).click(() => {
+    $("#cart-list").find(`#${this.event.target.id}`).closest(".row.align-items-start").remove();
+    removeProductFromCart(parseInt(this.event.target.id));
+  });
+
+  //bind event to add item from cart by plus button
+  cartElment.find(`#plus-${product.id}`).click(() => {
+    const productId = parseInt(this.event.target.id.split("-")[1]);
+    const prodCount = parseInt(productCount(productId));
+    $(`#${this.event.target.id}`)
+      .next(".count")
+      .text(`${prodCount + 1}`);
+    addProduct(productId);
+    $(`#price-${productId}`).text("$" + productTotalPice(productId));
+  });
+
+  //bind event to remove item from cart by minus button
+  cartElment.find(`#minus-${product.id}`).click(() => {
+    const productId = parseInt(this.event.target.id.split("-")[1]);
+    const prodCount = parseInt(productCount(productId));
+    $(`#${this.event.target.id}`)
+      .prev(".count")
+      .text(`${prodCount - 1}`);
+    removeProductFromCart(productId);
+    $(`#price-${productId}`).text("$" + productTotalPice(productId));
+    if (prodCount - 1 === 0) $("#cart-list").find(`#${productId}`).closest(".row.align-items-start").remove();
+  });
 }
 
 // get product cout by specific Id
@@ -240,6 +257,12 @@ function productCount(productId) {
   const cart = setUpCartObject();
 
   return cart.getProducts().filter((x) => x.id === productId).length;
+}
+
+function productTotalPice(productId) {
+  const cart = setUpCartObject();
+
+  return cart.showCartItems()?.find((x) => x.id == productId)?.price;
 }
 
 //remove items from cart
@@ -269,6 +292,105 @@ function setUpCartObject() {
   }
 
   return cart;
+}
+
+// checkout logic
+function checkOut() {
+  const cart = setUpCartObject();
+
+  if (cart.getProducts().length > 0) {
+    const cartModalElement = $("#cart-modal");
+    cartModalElement.empty();
+    cartModalElement.append(`
+        <table class="table table-sm table-striped">
+          <thead class="table-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th class="text-center" scope="col">Name</th>
+              <th class="text-center" scope="col">Quantity</th>
+              <th class="text-center" scope="col">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+      </table>
+    `);
+
+    let indexTable = 0;
+    for (const product of cart.showCartItems()) {
+      indexTable += 1;
+      const count = productCount(product.id);
+      cartModalElement.find("tbody").append(`
+        <tr>
+          <th scope="row">${indexTable}</th>
+          <td>${product.name}</td>
+          <td class="text-center">${count}</td>
+          <td class="text-end">${product.price}</td>
+        </tr>
+      `);
+    }
+
+    const total = cart.getTotalProductsPrice();
+    const discount = getDiscount(cart);
+    const totalToPay = total - discount;
+
+    cartModalElement.find("tbody").append(`
+      <tr>
+        <th scope="row"></th>
+        <td class="text-end" colspan="2"><b>Total:</b></td>
+        <td class="text-end"><b>$${total}</b></td>
+      </tr>
+      <tr>
+        <th scope="row"></th>
+        <td class="text-end" colspan="2"><b>Discount:</b></td>
+        <td class="text-end"><b>-$${discount}</b></td>
+      </tr>
+      <tr>
+        <th scope="row"></th>
+        <td class="text-end" colspan="2"><b>Total to pay:</b></td>
+        <td class="text-end"><b>$${totalToPay}</b></td>
+      </tr>
+  `);
+  }
+}
+
+// checkout logic
+function closeOrder() {
+  const finishOrderElement = $("#finish-order");
+
+  if (finishOrderElement.length > 0) {
+    const cart = setUpCartObject();
+    $.post("https://jsonplaceholder.typicode.com/posts", JSON.stringify(cart.getProducts()), function (res, state) {
+      if (state == "success") {
+        $("#checkout").modal("toggle");
+        clearCartOrder();
+        const cartModalElement = $("#cart-modal");
+        cartModalElement.empty();
+      }
+    });
+  }
+}
+
+// get discount
+function getDiscount(cart) {
+  let discount = 0.0;
+  let total = 0.0;
+  const products = cart.getProducts();
+
+  for (const product of products) total += product.price;
+
+  if (total > 1000) discount += 50.25;
+
+  if (total > 10000) discount += 99.99;
+
+  return discount;
+}
+
+function clearCartOrder() {
+  localStorage.removeItem(localkeys.cartProducts);
+  const cartItemElements = $("#cart-list > .align-items-start");
+  cartItemElements.remove();
+  showItemsCuantity();
 }
 
 $(() => {
@@ -364,10 +486,7 @@ $(() => {
   });
 
   $("#remove-all-item").click(() => {
-    localStorage.removeItem(localkeys.cartProducts);
-    const cartItemElements = $("#cart-list > .align-items-start");
-    cartItemElements.remove();
-    showItemsCuantity();
+    clearCartOrder();
   });
 
   //log out user event register
@@ -386,58 +505,3 @@ $(() => {
     }
   });
 });
-
-// function calculateDispatchPrice(zipCode) {
-//   let dipatchPrice = 0.0;
-//   switch (true) {
-//     case zipCode < 1000:
-//       dipatchPrice = 100.0;
-//       break;
-
-//     case zipCode >= 1000 && zipCode <= 2000:
-//       dipatchPrice = 200.0;
-//       break;
-
-//     case zipCode > 2000:
-//       dipatchPrice = 300.0;
-//       break;
-
-//     default:
-//       break;
-//   }
-
-//   return dipatchPrice;
-// }
-
-// function getDiscount(cart) {
-//   let discount = 0.0;
-//   const products = cart.getProducts();
-
-//   for (const product of products) {
-//     if (product.price > 1000) discount += 50.25;
-
-//     if (product.price > 10000) discount += 99.99;
-//   }
-
-//   return discount;
-// }
-
-// function checkOut(cart, zipCode) {
-//   const products = cart.getProducts();
-//   const productPrice = cart.getTotalProductsPrice();
-//   const dispatchCost = calculateDispatchPrice(zipCode);
-//   const discount = getDiscount(cart);
-//   const totalToPay = productPrice + dispatchCost - discount;
-
-//   let productsName = "";
-//   cart.showCartItems().forEach((element, index) => {
-//     if (index < products.length - 1) productsName += element.name + ", ";
-//     else productsName += element.name;
-//   });
-
-//   console.log(cart.showCartItems());
-
-//   alert(
-//     `Estimado ${firstname} ${lastname}. Total a pagar ${totalToPay} con estos productos: ${productsName}`
-//   );
-// }
